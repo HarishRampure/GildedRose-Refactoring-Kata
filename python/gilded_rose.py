a8fd414867341351a4fb19e1fb8ab7aab0c161de
HarishRampure <85104159+HarishRampure@users.noreply.github.com>
2024-06-05 08:31:01 +0530

    added logic for aged,sulfuras,backstage

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,5 +1,22 @@
 # -*- coding: utf-8 -*-
+"""
+Aged Brie increases in Quality the older it gets
+The Quality of an item is never more than 50
 
++5 Dexterity Vest TBD
+
+Elixir of the Mongoose TBD
+
+Sulfuras, Hand of Ragnaros, legendary item, never has to be sold or decreases in Quality
+
+Backstage passes to a TAFKAL80ETC like aged brie, increases in Quality as its SellIn value approaches;
+Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+Quality drops to 0 after the concert
+
+an item can never have its Quality increase above 50
+
+Conjured Mana Cake :TODO
+"""
 class GildedRose(object):
 
     def __init__(self, items):
@@ -7,6 +24,42 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
+            if item.name == "Aged Brie":
+                """
+                increases in Quality the older it gets
+                The Quality of an item is never more than 50
+                """
+                if item.quality < 50:
+                    item.quality += 1
+                if item.sell_in < 0 and item.quality < 50:
+                    item.quality += 1
+
+            elif item.name == "Sulfuras, Hand of Ragnaros":
+                """
+                legendary item, never has to be sold or decreases in Quality
+                """
+                pass
+            
+            elif item.name == "Backstage passes to a TAFKAL80ETC":
+                """
+                like aged brie, increases in Quality as its SellIn value approaches;
+                Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+                Quality drops to 0 after the concert
+                """
+                if item.quality < 50:
+                    item.quality += 1
+                    if item.sell_in < 11:
+                        item.quality += 1
+                    if item.sell_in < 6:
+                        item.quality += 1
+                if item.sell_in < 0:
+                    item.quality = 0
+
+            elif item.name == "Conjured Mana Cake":
+                pass
+            else:
+                pass
+
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":

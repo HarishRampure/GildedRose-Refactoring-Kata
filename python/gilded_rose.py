368a01a9aa5a48f47c202af07e8971d46026a870
HarishRampure <Harishamzr@gmail.com>
2025-02-28 15:29:03 +0530

    logic update/test case for conjured item

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -6,35 +6,31 @@ class GildedRose(object):
         self.items = items
 
     def update_quality(self):
+        """
+        Updates 'sell_in' and 'quality' for all items, including new conjured items.
+        """
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            #Sulfuras does not change.
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                continue
+            # Decrease sell_in for all non-Sulfuras items.
+            item.sell_in -= 1
+            # Base degrade rate for a "normal" item
+            degrade_rate = 1
+            # Conjured items degrade in Quality twice as fast as normal items
+            if "Conjured" in item.name:
+                degrade_rate *= 2
+            self._update_normal_item(item, degrade_rate)
+            if item.quality < 0:
+                item.quality = 0
+            if item.quality > 50 and item.name != "Sulfuras, Hand of Ragnaros":
+                item.quality = 50
 
+    def _update_normal_item(self, item, degrade_rate):
+        item.quality -= degrade_rate
+        # If past sell_in date, degrade twice again
+        if item.sell_in < 0:
+            item.quality -= degrade_rate
 
 class Item:
     def __init__(self, name, sell_in, quality):
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,11 +5,17 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    # def test_foo(self):
+    #     items = [Item("foo", 0, 0)]
+    #     gilded_rose = GildedRose(items)
+    #     gilded_rose.update_quality()
+    #     self.assertEqual("fixme", items[0].name)
+
+    def test_conjured_items_degrade_twice_as_fast(self):
+        items = [Item("Conjured Mana Cake", 5, 10)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+        assert items[0].quality == 8
 
         
 if __name__ == '__main__':

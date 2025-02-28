8e5ad900cfdff352cf9a110ba05bcf73dda637ed
HarishRampure <Harishamzr@gmail.com>
2025-02-28 15:50:06 +0530

    added logic for Aged Brie

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
 
+# Once the sell by date has passed, Quality degrades twice as fast
+# The Quality of an item is never negative
+# "Aged Brie" actually increases in Quality the older it gets
+# The Quality of an item is never more than 50
+# "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+# "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+# Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+# Quality drops to 0 after the concert
+# We have recently signed a supplier of conjured items. This requires an update to our system:
+# "Conjured" items degrade in Quality twice as fast as normal items
+
 class GildedRose(object):
 
     def __init__(self, items):
@@ -10,6 +21,7 @@ class GildedRose(object):
         Updates 'sell_in' and 'quality' for all items, including new conjured items.
         """
         for item in self.items:
+            
             #Sulfuras does not change.
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
@@ -20,11 +32,30 @@ class GildedRose(object):
             # Conjured items degrade in Quality twice as fast as normal items
             if "Conjured" in item.name:
                 degrade_rate *= 2
-            self._update_normal_item(item, degrade_rate)
+            
+            if item.name == "Aged Brie":
+                self._update_aged_brie(item)
+            elif item.name.startswith("Backstage passes"):
+                self._update_backstage_pass(item)
+            else:
+                # all other items
+                self._update_normal_item(item, degrade_rate)
+
             if item.quality < 0:
                 item.quality = 0
             if item.quality > 50 and item.name != "Sulfuras, Hand of Ragnaros":
                 item.quality = 50
+
+    def _update_aged_brie(self, item):
+        # Increase quality by 1
+        item.quality += 1
+        # increases in Quality the older it gets
+        if item.sell_in < 0:
+            item.quality += 1
+
+
+    def _update_backstage_pass(self, item):
+        pass
 
     def _update_normal_item(self, item, degrade_rate):
         item.quality -= degrade_rate
